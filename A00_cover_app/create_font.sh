44b249a3f8965301150d1111ff3dff36fed5bc39
Yuma Ohgami <yuma@ohgami.jp>
2018-04-06 23:00:09 +0900

    edit

--- A00_cover_app/create_font.sh
+++ A00_cover_app/create_font.sh
@@ -7,9 +7,12 @@ src=$1
 pbm=$(convert $src -compress none pbm:- | grep -v '^#' | tail -n +3 | tr -d '\n')
 size=$(echo -n $pbm | wc -c)
 
+echo -e '\t{'
 while [ $size -gt 0 ]; do
-        echo -n $pbm | head -c $((WIDTH * 2))
-        size=$((size - (WIDTH * 2)))
-        pbm=$(echo -n $pbm | tail -c $size)
-        echo
+	echo -ne '\t\t{'
+	echo -n $pbm | head -c $((WIDTH * 2 - 1)) | tr ' ' ','
+	size=$((size - (WIDTH * 2)))
+	pbm=$(echo -n $pbm | tail -c $size)
+	echo '},'
 done
+echo -e '\t},'
