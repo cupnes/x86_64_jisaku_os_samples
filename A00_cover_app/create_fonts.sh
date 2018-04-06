3d74ef3adfb98c64d4ffaf7dd84a3e2cd777dc8b
Yuma Ohgami <yuma@ohgami.jp>
2018-04-07 00:45:02 +0900

    edit

--- A00_cover_app/create_fonts.sh
+++ A00_cover_app/create_fonts.sh
@@ -1,13 +1,45 @@
 #!/bin/bash
 
+SRC_DIR=~/x86_64_poios/materials/cover_font/64x64
+DST_DIR=out
+
 WIDTH=64
 HEIGHT=64
-CREATE_FONT=~/x86_64_jisaku_os_samples/A00_cover_app/create_font.sh
+CREATE_FONT=./create_font.sh
 
+mkdir -p ${DST_DIR}
+
+# font.h
+cat >${DST_DIR}/font.h <<EOF
+#ifndef _FONT_H_
+#define _FONT_H_
+
+#define FONT_WIDTH	${WIDTH}
+#define FONT_HEIGHT	${HEIGHT}
+
+enum FONT_IDX {
+EOF
+for s in $(ls ${SRC_DIR}); do
+	echo -e "\tFONT_$(echo $s | rev | cut -d'.' -f2- | rev),"
+done >>${DST_DIR}/font.h
+cat >>${DST_DIR}/font.h <<EOF
+};
+
+extern const unsigned char font_bitmap[][FONT_HEIGHT][FONT_WIDTH];
+
+#endif
+EOF
+
+# font.c
+echo '#include <font.h>' >${DST_DIR}/font.c
+echo >>${DST_DIR}/font.c
+echo 'const unsigned char font_bitmap[][FONT_HEIGHT][FONT_WIDTH] = {' >>${DST_DIR}/font.c
 idx=0
-for s in $(ls ${WIDTH}x${HEIGHT}); do
-	echo -e "\t{\t/* $idx: $(echo $s | rev | cut -d'.' -f2- | rev) */"
-	${CREATE_FONT} ${WIDTH}x${HEIGHT}/$s
+for s in $(ls ${SRC_DIR}); do
+	name=$(echo $s | rev | cut -d'.' -f2- | rev)
+	echo -e "\t[FONT_$name] = {\t/* $idx: $name */"
+	${CREATE_FONT} ${SRC_DIR}/$s
 	echo -e '\t},'
 	idx=$((idx + 1))
-done
+done >>${DST_DIR}/font.c
+echo '};' >>${DST_DIR}/font.c
--- A00_cover_app/main.c
+++ A00_cover_app/main.c
@@ -32,10 +32,10 @@ void start_kernel(void *_t __attribute__ ((unused)), struct framebuffer *_fb,
 	/* 画像ビューアの初期化 */
 	/* iv_init(); */
 
-	putc(font_kan_ji);
-	putc(font_kan_saku);
-	putc(font_O);
-	putc(font_S);
+	putc(FONT_kan_ji);
+	putc(FONT_kan_saku);
+	putc('O');
+	putc('S');
 
 	/* CPUの割り込み有効化 */
 	enable_cpu_intr();
