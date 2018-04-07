2cab7d4dbc72148c1a9cf8f56511017acf81dc32
Yuma Ohgami <yuma@ohgami.jp>
2018-04-07 12:01:01 +0900

    edit

--- A00_cover_app/main.c
+++ A00_cover_app/main.c
@@ -32,7 +32,22 @@ void start_kernel(void *_t __attribute__ ((unused)), struct framebuffer *_fb,
 	/* 画像ビューアの初期化 */
 	/* iv_init(); */
 
-	move_cursor(100, 50);
+	/* move_cursor(100, 50); */
+	/* フルスクラッチで作る! */
+	putc(FONT_kata_fu);
+	putc(FONT_kata_ru);
+	putc(FONT_kata_su);
+	putc(FONT_kata_ku);
+	putc(FONT_kata_ra);
+	putc(FONT_kata_xtu);
+	putc(FONT_kata_chi);
+	putc(FONT_hira_de);
+	putc(FONT_kan_saku);
+	putc(FONT_hira_ru);
+	putc('!');
+
+	puts("\r\n");
+
 	putc(FONT_kan_ji);
 	putc(FONT_kan_saku);
 	putc('O');
